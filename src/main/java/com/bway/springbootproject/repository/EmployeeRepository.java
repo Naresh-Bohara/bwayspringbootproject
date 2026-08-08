1b3d5f0596cbd349dce447cac9ba6a803bad40ca
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-08-08 21:28:37 +0545

    updated

---
+++ src/main/java/com/bway/springbootproject/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.bway.springbootproject.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.bway.springbootproject.model.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+	
+}
---
+++ src/main/java/com/bway/springbootproject/service/EmployeeService.java
@@ -0,0 +1,5 @@
+package com.bway.springbootproject.service;
+
+public interface EmployeeService {
+	
+}
