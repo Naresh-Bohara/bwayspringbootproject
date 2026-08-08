c859b5da0b23b32aa5cc0c8911e5e0b71e9c9dc6
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-08-08 14:07:00 +0545

    code added

---
+++ src/test/java/com/bway/springbootproject/BwayspringbootprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bway.springbootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BwayspringbootprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
