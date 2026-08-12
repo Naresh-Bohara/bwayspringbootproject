1d645d92e9f7868dc2be9dd992ccadb45c8ae33d
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-08-12 14:14:56 +0545

    updated to rest api

--- src/main/java/com/bway/springbootproject/api/EmployeeRestController.java
+++ src/main/java/com/bway/springbootproject/api/EmployeeRestController.java
@@ -1,5 +1,51 @@
 package com.bway.springbootproject.api;
 
-public class EmployeeRestController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bway.springbootproject.model.Employee;
+import com.bway.springbootproject.service.EmployeeService;
+
+@RestController
+public class EmployeeRestController {
+	@Autowired
+	private EmployeeService empService;
+	@GetMapping("/api/emp/list")
+	public List<Employee> getEmps() {
+		return empService.getAllEmps();
+	}
+	
+	@PostMapping("/api/emp/add")
+	public String add(@RequestBody Employee emp) {
+		
+		empService.addEmp(emp);
+		return "added success!";
+	}
+	
+	@GetMapping("/api/emp/{id}")
+	public Employee getOne(@PathVariable long id) {
+		return empService.getEmpById(id);
+	}
+	
+	@DeleteMapping("/api/emp/delete{id}")
+	public String delete(@PathVariable long id) {
+		empService.deleteEmp(id);
+		return "delete success!";
+	}
+	@PutMapping("/api/empp/update{id}")
+	public String update(@RequestBody Employee emp) {
+		empService.updateEmp(emp);
+		return "";
+	}
+	
+	
+	
 }
